d12a0d42853838beca6c8fc7ae6e68125570bb91
truongbui <btxtruong@gmail.com>
2024-08-17 11:56:26 +0700

    feat: add favorite

--- lib/components/restaurant_landscape_card.dart
+++ lib/components/restaurant_landscape_card.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 
 import '../models/models.dart';
 
-class RestaurantLandscapeCard extends StatelessWidget {
+class RestaurantLandscapeCard extends StatefulWidget {
   final Restaurant restaurant;
 
   const RestaurantLandscapeCard({
     super.key,
     required this.restaurant,
   });
+
+  @override
+  State<RestaurantLandscapeCard> createState() => _RestaurantLandscapeCardState();
+}
+
+class _RestaurantLandscapeCardState extends State<RestaurantLandscapeCard> {
+
+  bool _isFavorited = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +33,42 @@ class RestaurantLandscapeCard extends StatelessWidget {
             ),
             child: AspectRatio(
               aspectRatio: 2,
-              child: Image.asset(
-                restaurant.imageUrl,
-                fit: BoxFit.cover,
-              ),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.asset(
+                    widget.restaurant.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    top: 4.0,
+                    right: 4.0,
+                    child: IconButton(
+                      icon: Icon(
+                        _isFavorited
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                      ),
+                      iconSize: 30.0,
+                      color: Colors.red[400],
+                      onPressed: () {
+                        setState(() {
+                          _isFavorited = !_isFavorited;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              )
             ),
           ),
           ListTile(
             title: Text(
-              restaurant.name,
+              widget.restaurant.name,
               style: textTheme.titleSmall,
             ),
             subtitle: Text(
-              restaurant.attributes,
+              widget.restaurant.attributes,
               maxLines: 1,
               style: textTheme.bodySmall,
             ),
